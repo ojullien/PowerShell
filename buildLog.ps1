50616cd692c63d863c27cd0f1f6636bc6c690c8b
ojullien <3778194+ojullien@users.noreply.github.com>
2018-04-23 21:50:00 +0200

    initial commit

---
+++ buildLog.ps1
@@ -0,0 +1,41 @@
+<#PSScriptInfo
+
+.VERSION 1.0.0
+
+.GUID a5f80ea2-7d17-43ce-adeb-a501ee4fed5d
+
+.AUTHOR Olivier Jullien
+
+.COMPANYNAME
+
+.COPYRIGHT MIT License
+
+.TAGS
+
+.LICENSEURI https://github.com/ojullien/powershell/blob/master/LICENSE
+
+.PROJECTURI https://github.com/ojullien/PowerShell
+
+.ICONURI
+
+.EXTERNALMODULEDEPENDENCIES
+
+.REQUIREDSCRIPTS .
+
+.EXTERNALSCRIPTDEPENDENCIES
+
+.RELEASENOTES
+Date: 20180423
+Powershell Version: 5.1
+
+#>
+
+<#
+
+.DESCRIPTION
+ build consistent log from many apache log
+
+#>
+Param()
+
+
